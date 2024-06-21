b9453ff0528e9dab82b2da36e2714306e816d701
Ajay Nallanagula <ajay.nallanagulla@ukg.com>
2024-06-21 18:59:17 +0530

    single-spa utility-microapp

---
+++ src/org-utilityts-singlespa-utilityts.ts
@@ -0,0 +1,9 @@
+// Anything exported from this file is importable by other in-browser modules.
+
+export function publicApiFunction(): string {
+  return " This is  publicApiFunction coming from utility-microapp";
+}
+
+export function testApiFunction(): string {
+  return " This is testApiFunction coming from utility-microapp";
+}
